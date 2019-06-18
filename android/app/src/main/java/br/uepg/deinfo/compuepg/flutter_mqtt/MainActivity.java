f4fb44b7eb52680ecee3e2dee39cabd921507f86
Pedro Neto <pedromneto97@gmail.com>
2019-06-18 19:13:47 -0300

    Initial commit

---
+++ android/app/src/main/java/br/uepg/deinfo/compuepg/flutter_mqtt/MainActivity.java
@@ -0,0 +1,13 @@
+package br.uepg.deinfo.compuepg.flutter_mqtt;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
