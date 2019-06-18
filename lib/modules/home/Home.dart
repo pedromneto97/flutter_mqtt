e4ea51186776c61de098861a45c964ec8c611d4b
Pedro Neto <pedromneto97@gmail.com>
2019-06-18 19:14:49 -0300

    Add: Home

---
+++ lib/modules/home/Home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mqtt/modules/Scaffold/Scaffold.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return MyScaffold(
+      Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SvgPicture.asset('images/logo.svg'),
+          ],
+        ),
+      ),
+    );
+  }
+}
