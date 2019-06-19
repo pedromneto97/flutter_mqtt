2d5eaa57453107984d29f5d13620a50e31688707
Pedro Neto <pedromneto97@gmail.com>
2019-06-19 19:26:07 -0300

    Update: Lista de chaves e valores para publicar a informação

--- lib/modules/mqqt_operations/mqtt_publish_form.dart
+++ lib/modules/mqqt_operations/mqtt_publish_form.dart
@@ -7,13 +7,16 @@ class MqttPublishForm extends StatefulWidget {
 
 class _MqttPublishForm extends State<MqttPublishForm> {
   TextEditingController _topicController;
-  List<List<TextEditingController>> _pairs;
+  List<List<TextEditingController>> _pairs =
+  new List<List<TextEditingController>>();
   final GlobalKey<FormState> _key = GlobalKey<FormState>();
+  List<Row> items;
 
   @override
   void initState() {
     super.initState();
     _topicController = TextEditingController();
+    _pairs.add([TextEditingController(), TextEditingController()]);
   }
 
   @override
@@ -33,31 +36,89 @@ class _MqttPublishForm extends State<MqttPublishForm> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Flexible(
-            child: TextFormField(
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Tópico do MQTT';
-                }
-                return null;
-              },
-              decoration: InputDecoration(labelText: "Tópico"),
-              controller: _topicController,
-              keyboardType: TextInputType.text,
-            ),
-            fit: FlexFit.loose,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: RaisedButton(
-              onPressed: () =>
-                  _key.currentState.validate() ? _onSubmit() : null,
-              child: Text('Submit'),
-            ),
-          ),
-        ],
+        children: buildItems(),
       ),
     );
+  }
+
+  List<Widget> buildItems() {
+    List<Widget> widgets = [
+      Flexible(
+        child: TextFormField(
+          validator: (value) {
+            if (value.isEmpty) {
+              return 'Tópico do MQTT';
+            }
+            return null;
+          },
+          decoration: InputDecoration(labelText: "Tópico"),
+          controller: _topicController,
+          keyboardType: TextInputType.text,
+        ),
+        fit: FlexFit.loose,
+      ),
+    ];
+    _pairs.forEach((list) {
+      widgets.add(
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Flexible(
+              child: TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Chave';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(labelText: "Chave"),
+                controller: list.first,
+                keyboardType: TextInputType.text,
+              ),
+              fit: FlexFit.loose,
+            ),
+            Flexible(
+              child: TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Valor';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(labelText: "Valor"),
+                controller: list.last,
+                keyboardType: TextInputType.text,
+              ),
+              fit: FlexFit.loose,
+            ),
+          ],
+        ),
+      );
+    });
+    widgets.add(Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: RaisedButton(
+            onPressed: () =>
+                this.setState(() =>
+                    this
+                        ._pairs
+                        .add(
+                        [TextEditingController(), TextEditingController()])),
+            child: Text('Add Pair'),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: RaisedButton(
+            onPressed: () => _key.currentState.validate() ? _onSubmit() : null,
+            child: Text('Submit'),
+          ),
+        ),
+      ],
+    ));
+    return widgets;
   }
 }
